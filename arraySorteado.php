5e3707e08ecc9c12d63399695b6ace7391927764
Tiago <jamestk93@gmail.com>
2022-01-12 22:29:22 -0300

    up

--- arraySorteado.php
+++ arraySorteado.php
@@ -1,8 +1,11 @@
 <?php
 
-$array = [9,9,1,20];
+for ($i=1; $i <= 20; $i++) { 
+    $arr_sorteado[] = rand(1,10);
+}
+print_r($arr_sorteado);
 
-$array = array_count_values($array);
+$array = array_count_values($arr_sorteado);
 
 foreach ($array as $key => $value) {    
     if ($value == 1) {
